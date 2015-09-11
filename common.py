b1a3d2eee54b3b4d33bd545cca4c09cd77ff122d
afazzari <afazzari@sensoft.ca>
2015-09-11 15:14:43 -0400

    Added decorator for filtering the type of channel in which the message was received

--- common.py
+++ common.py
@@ -28,3 +28,20 @@ def mentioned(func):
 
     return func_wrapper
 
+def channel_type(channel_type):
+    def _channel_type(func):
+        """
+        Returns the function if the channel is a direct message to the bot
+        :param func:
+        :return:
+        """
+        def func_wrapper(message):
+            channel_types = [c.lower()[0] for c in channel_type.split("+")]
+            if message['channel'].lower()[0] in channel_types:
+                func(message)
+            else:
+                pass
+
+        return func_wrapper
+    return _channel_type
+
