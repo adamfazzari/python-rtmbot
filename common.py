dc26a19a71b5b24dc3f3a9730f78b10487c0b6c2
afazzari <afazzari@sensoft.ca>
2015-09-15 12:16:08 -0400

    Changed for_me to command Added disabled decorator to quickly disable plugins without removing from the folder

--- common.py
+++ common.py
@@ -1,18 +1,40 @@
+import re
+
 MY_NAME = 'stacey'
 
-def for_me(func):
+def disabled(func):
     """
-    Returns the function if the bot's name was mentioned at the start of the message
+    Temporarily disable a plugin
     :param func:
     :return:
     """
     def func_wrapper(message):
-        if message['text'].lower().startswith(MY_NAME):
-            func(message)
-        else:
-            pass
+        pass
 
     return func_wrapper
+
+def command(command):
+    def _command(func):
+        """
+        Returns the function if the bot's name was mentioned at the start of the message followed by the specified command
+        :param func:
+        :return: The message text with the salutation removed
+        """
+        def func_wrapper(message):
+            if message['text'].lower().startswith(' '.join([MY_NAME, 'help'])):
+                msg = dict(message)
+                msg['text'] = re.sub("^{}".format(MY_NAME), '', msg['text']).strip()
+                msg['text'] = 'help'
+                func(msg)
+            elif message['text'].lower().startswith(' '.join([MY_NAME, command])):
+                msg = dict(message)
+                msg['text'] = re.sub("^{}".format(MY_NAME), '', msg['text']).strip()
+                func(msg)
+            else:
+                pass
+
+        return func_wrapper
+    return _command
 
 def mentioned(func):
     """
